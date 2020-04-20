c34468b0d2e72d46e9544b973dfba84d6c7369ee
Nina <nina.kaveh1998@gmail.com>
2020-04-20 10:28:41 +0200

    Add header/footer and some CSS

---
+++ web/js/includeHeaderFooter.js
@@ -0,0 +1,20 @@
+function addGlobalHeader(){
+    $(function(){
+        $("#globalHeader").load("globalHeader.html");
+    });
+}
+function addGlobalFooter(){
+    $(function(){
+        $("#globalFooter").load("globalFooter.html");
+    });
+}
+function addUserHeader(){
+    $(function(){
+        $("#userHeader").load("userHeader.html");
+    });
+}
+function addUserFooter(){
+    $(function(){
+        $("#userFooter").load("userFooter.html");
+    });
+}
